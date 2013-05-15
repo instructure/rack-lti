c610c3edc4fab58a634133a3e5b7242a3ab56b69
Zach Pendleton <zachpendleton@gmail.com>
2013-05-15 07:17:48 -0600

    retab middleware.rb

--- lib/rack/lti/middleware.rb
+++ lib/rack/lti/middleware.rb
@@ -3,58 +3,58 @@ require 'oauth/request_proxy/rack_request'
 require 'rack/lti/config'
 
 module Rack::LTI
-	class Middleware
-		attr_reader :app, :config
+  class Middleware
+    attr_reader :app, :config
 
-		def initialize(app, options = {}, &block)
-			@app    = app	
-			@config = Config.new(options, &block)
-		end
+    def initialize(app, options = {}, &block)
+      @app    = app 
+      @config = Config.new(options, &block)
+    end
 
-		def call(env)
-			request = Rack::Request.new(env)
+    def call(env)
+      request = Rack::Request.new(env)
 
-			if routes.has_key?(request.path)
-				env['rack.lti'] = true
-				send(routes[request.path], request, env)
+      if routes.has_key?(request.path)
+        env['rack.lti'] = true
+        send(routes[request.path], request, env)
       else
         @app.call(env)
-			end
-		end
+      end
+    end
 
-		def routes
-			{
-				@config.config_path => :config_action,
-				@config.launch_path => :launch_action
-			}
-		end
+    def routes
+      {
+        @config.config_path => :config_action,
+        @config.launch_path => :launch_action
+      }
+    end
 
     private
 
-		def config_action(request, env)
+    def config_action(request, env)
       response = [@config.to_xml(launch_url: request.url.sub(@config.config_path, @config.launch_path))]
       [200, { 'Content-Type' => 'application/xml', 'Content-Length' => response[0].length.to_s }, response]
-		end
+    end
 
-		def launch_action(request, env)
-			provider = IMS::LTI::ToolProvider.new(@config.consumer_key(*request.params.values_at('oauth_consumer_key', 'tool_consumer_instance_guid')),
-																						@config.consumer_secret(*request.params.values_at('oauth_consumer_key', 'tool_consumer_instance_guid')),
-																						request.params)
+    def launch_action(request, env)
+      provider = IMS::LTI::ToolProvider.new(@config.consumer_key(*request.params.values_at('oauth_consumer_key', 'tool_consumer_instance_guid')),
+                                            @config.consumer_secret(*request.params.values_at('oauth_consumer_key', 'tool_consumer_instance_guid')),
+                                            request.params)
 
-			if valid?(provider, request)
+      if valid?(provider, request)
         @config.success.call(provider.to_params, env['rack.session'])
         [301, { 'Content-Length' => '0', 'Content-Type' => 'text/html', 'Location' => @config.app_path }, []]
-			else
+      else
         response = 'Invalid launch.'
         [403, { 'Content-Type' => 'text/plain', 'Content-Length' => response.length.to_s }, [response]]
-			end
-		end
+      end
+    end
 
-		def valid?(provider, request)
-			valid_request?(provider, request) &&
+    def valid?(provider, request)
+      valid_request?(provider, request) &&
         valid_nonce?(request.params['oauth_nonce']) &&
         valid_timestamp?(request.params['oauth_timestamp'].to_i)
-		end
+    end
 
     def valid_request?(provider, request)
       @config.public? ? true : provider.valid_request?(request)
@@ -75,5 +75,5 @@ module Rack::LTI
         (Time.now.to_i - @config.time_limit) <= timestamp
       end
     end
-	end
+  end
 end
